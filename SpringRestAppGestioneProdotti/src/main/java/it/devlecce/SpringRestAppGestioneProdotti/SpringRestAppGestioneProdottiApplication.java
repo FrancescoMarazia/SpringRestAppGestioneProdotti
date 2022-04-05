109ad79a4ce53c089b723f79745bf2d2e0c3f681
FM <fmarazia@studenti.apuliadigitalmaker.it>
2022-04-05 20:48:56 +0200

    Primo Commit

---
+++ SpringRestAppGestioneProdotti/src/main/java/it/devlecce/SpringRestAppGestioneProdotti/SpringRestAppGestioneProdottiApplication.java
@@ -0,0 +1,13 @@
+package it.devlecce.SpringRestAppGestioneProdotti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAppGestioneProdottiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAppGestioneProdottiApplication.class, args);
+	}
+
+}
